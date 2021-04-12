dbc00dfcc8c9e1dacb017510fc44c16b81cb08a8
KaijuML <clement.rebuffel@lip6.fr>
2021-04-12 16:06:29 +0200

    Adding inference script to run.py

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from onmt.bin.preprocess import main as preprocess
+from onmt.bin.inference import main as inference
 from onmt.bin.train import main as train
 import argparse
 
@@ -13,14 +14,14 @@ if __name__ == '__main__':
     parser._remove_action(help_action)
     [parser._option_string_actions.pop(s) for s in help_action.option_strings]
 
-    # Simply add an argument for preprocess, train, translate
+    # Simply add an argument for preprocess, train, inference
     mode = parser.add_mutually_exclusive_group()
     mode.add_argument("--preprocess", dest='preprocess', action='store_true',
-                      help="Activate to preprocess with OpenNMT")
+                      help="Preprocess data")
     mode.add_argument("--train", dest='train', action='store_true',
-                      help="Activate to train with OpenNMT")
-    mode.add_argument("--translate", dest='translate', action='store_true',
-                      help="Activate to translate with OpenNMT")
+                      help="Run training script")
+    mode.add_argument("--inference", dest='inference', action='store_true',
+                      help="Run inference script using a trained model")
 
     group = parser.add_argument_group('Help')
     group.add_argument('--help', dest='help', action='store_true')
@@ -31,7 +32,7 @@ if __name__ == '__main__':
         preprocess(remaining_args)
     elif current_args.train:
         train(remaining_args)
-    elif current_args.translate:
-        raise ValueError('Cannot train/translate for now!')
+    elif current_args.inference:
+        inference(remaining_args)
     else:
         help_action(parser, current_args, None)
